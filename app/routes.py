8ec09b0133a9703e952067b4311dc0e6ec77bddd
KellyB <38035824+butlerk@users.noreply.github.com>
2021-10-22 12:14:22 +1100

    Added security
    changed passwords to be in local .env file
    added login_required and admin_required functionality and changed views.

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from flask import render_template, redirect, url_for
-
+from flask_login import login_required
 from app import app
 from app.models import Pet
 
 @app.route('/')
+@login_required
 def index():
     pets = Pet.query.all()
     return render_template('index.html', title = 'Home', pets = pets)
